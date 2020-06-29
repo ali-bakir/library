30ef375ba9c32be72025e2749ec107ed1657780b
alibakr <alibakrpeyzaj@gmail.com>
2020-06-29 22:29:21 +0300

    add_favourite, delete_favourite, add_wish changed

--- book/urls.py
+++ book/urls.py
@@ -1,24 +1,24 @@
 from django.urls import path
 from . import views
-from .views import BookView, FavouriteBookView
+from .views import BookView, FavouriteBookView, DashboardView, WishBookView
 
 app_name = "book"
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name="dashboard"),
+    path('dashboard/', DashboardView.as_view(), name="dashboard"),
     #path('add_book/', views.add_book, name="add_book"),
-    path('update/<pk>/', views.BookView.as_view(), name="update"),
+    path('update/<int:pk>/', views.BookView.as_view(), name="update"),
     path('book/<int:id>', views.detail, name="detail"),
-    path('delete/<int:id>', views.delete_book, name="delete"),
+    path('delete/<int:pk>', BookView.as_view(), name="delete"),
     path('', BookView.as_view(), name="books"),
     path('comment/<int:id>', views.comment, name="comment"),
-    path('add_favourite/<pk>', FavouriteBookView.as_view(), name="add_favourite"),
-    path('delete_favourite/<pk>', FavouriteBookView.as_view(),
-         name="delete_favourite"),
-    path('add_wish/', views.add_wish, name="add_wish"),
     path('favourite/', FavouriteBookView.as_view(), name="favourite"),
-    path('wish/', views.wish, name="wish"),
-    path('delete_wish/<int:id>', views.delete_wish,
+    #path('delete_favourite/<int:pk>', FavouriteBookView.as_view(),
+         #name="delete_favourite"),
+    path('add_wish/', WishBookView.as_view(), name="add_wish"),
+    #path('favourite/', FavouriteBookView.as_view(), name="favourite"),
+    path('wish/', WishBookView.as_view(), name="wish"),
+    path('delete_wish/<int:pk>', WishBookView.as_view(),
          name="delete_wish"),
 
 ]
