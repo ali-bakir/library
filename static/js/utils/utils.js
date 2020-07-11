0f18a25894d7f7ad4faa10561b580b417264dfea
alibakr <alibakrpeyzaj@gmail.com>
2020-07-11 22:34:42 +0300

    add utils.js

---
+++ static/js/utils/utils.js
@@ -0,0 +1,10 @@
+let getFormData = function (form) {
+    let unindexed_array = form.serializeArray();
+    let indexed_array = {};
+
+    $.map(unindexed_array, function (n, i) {
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
