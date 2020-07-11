f6dc9b40fec8cec07e3593dab25ebfb10b51672f
alibakr <alibakrpeyzaj@gmail.com>
2020-07-11 22:33:26 +0300

    added DetailView and CommentView

--- library/urls.py
+++ library/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from book.views import IndexView, BookView, AboutView
+from book.views import IndexView, BookView, AboutView, DetailView, CommentView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
