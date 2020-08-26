88466c7f8a4f2cd685bfd6b3dd56866c47931cfe
alibakr <alibakrpeyzaj@gmail.com>
2020-08-26 18:37:30 +0300

    project pertama

--- static/js/book/book.js
+++ static/js/book/book.js
@@ -1,66 +1,25 @@
+import {getFormData} from "../utils/utils";
+
 $(document).ready(function () {
-    $('#id_comment_post').click(function (event) {
+    $('#id_comment_post').submit(function (event) {
+        console.log("gürol çay içti")
+        event.preventDefault();
+        let form = $("form.js-book-comment");
+        let book_comment_url = form.attr("book-comment-url");
+        let book_id = form.attr("book-id");
+        console.log(form)
+        let form_data = getFormData(form)
 
-            let form = $("form.js-book-comment");
-            let book_comment_url = form.attr("book-comment-url");
-            let book_id = form.attr("book-id");
-            let form_data = getFormData(form)
-
-            form_data['book'] = book_id
-
-            $.post(
-                book_comment_url,
-                form_data,
-                function (resp) {
-                    console.log(resp)
-                }
-            )
-
-            // $.ajax({
-            //     url: book_comment_url,
-            //     type: 'POST',
-            //     data: form_data,
-            //     dataType: 'json',
-            //     success: function (resp) {
-            //         console.log(resp);
-            //     },
-            //     error: function (err) {
-            //         console.log(err);
-            //     }
-            // })
-
-        }
-    );
-});
-
-let getFormData = function (form) {
-    let unindexed_array = form.serializeArray();
-    let indexed_array = {};
-
-    $.map(unindexed_array, function (n, i) {
-        indexed_array[n['name']] = n['value'];
-    });
-
-    return indexed_array;
-}
-
-
-// let submitBookComment = function (book_id) {
-//     var form = $(this).closest("form");
-//     // let username = document.getElementById("id_comment_form_name").value;
-//     // let comment = document.getElementById("id_comment_form_comment").value;
-//     event.preventDefault();
-//     console.log(book_id)
-//     console.log(form.serialize())
-//     console.log(form.attr("book-comment-url"))
-//     // $.ajax({
-//     //     url: form.attr("book-comment-url"),
-//     //     data: form.serialize(),
-//     //     dataType: 'json',
-//     //     success: function (data) {
-//     //       if (data.is_taken) {
-//     //         alert(data.error_message);
-//     //       }
-//     //     }
-//     //   });
-// }
+        // form_data['book'] = book_id
+        console.log(form_data)
+        console.log(book_comment_url)
+        //
+        // $.post(
+        //     book_comment_url,
+        //     form_data,
+        //     function (resp) {
+        //         console.log(resp)
+        //     }
+        // )
+    })
+})
--- static/js/utils/utils.js
+++ static/js/utils/utils.js
@@ -1,10 +1,11 @@
-let getFormData = function (form) {
+ export function getFormData(form) {
     let unindexed_array = form.serializeArray();
     let indexed_array = {};
 
-    $.map(unindexed_array, function (n, i) {
-        indexed_array[n['name']] = n['value'];
-    });
+    // $.map(unindexed_array, function (n, i) {
+    //     indexed_array[n['name']] = n['value'];
+    // });
 
     return indexed_array;
 }
+
